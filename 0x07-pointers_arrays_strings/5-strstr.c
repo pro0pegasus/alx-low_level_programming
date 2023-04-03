1229cf3474fcc61c1e45e1a28cbafe81410324c9
Yassine Borak <yassineborak@gmail.com>
2023-04-03 13:48:59 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strncmp - compares two strings within b bytes.
  * @s1: string to check
@@ -10,6 +11,7 @@
 int _strncmp(char *s1, char *s2, unsigned int b)
 {
 	unsigned int i;
+
 	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
 	{
 		s1++;
