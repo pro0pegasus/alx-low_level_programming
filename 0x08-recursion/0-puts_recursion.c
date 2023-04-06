00802ece368e122b7d6d22074ef2560cbf748faf
Yassine Borak <yassineborak@gmail.com>
2023-04-06 09:04:14 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,13 @@
 
 /**
  * _puts_recursion - Prints a string followed by a new line
- * @s: string
+ * @s: The string to be printed
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
