fce84bfade3b6787d56764e5ecd96e0ee5d41ac0
BORAK <yassineborak@gmail.com>
2023-05-01 15:37:10 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        num++;
-    }
-    return num;
- }
+	const listint_t *current_node = h;
+	size_t num = 0;
+
+	while (current_node != NULL)
+	{
+		printf("%d\n", current_node->n);
+		num++;
+		current_node = current_node->next;
+	}
+
+	return (num);
+}
